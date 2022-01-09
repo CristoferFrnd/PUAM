f6846ed706a9fc3ba9fcc8893831e43824061ba5
cfeliot <cristoferfrnd@gmail.com>
2022-01-08 19:46:06 -0500

    Corrección seguridad

--- helpers/recuperar.php
+++ helpers/recuperar.php
@@ -5,6 +5,7 @@ use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 // Load Composer's autoloader
 require '../vendor/autoload.php';
+require '../config.php';
 
 $email = $_POST['correo'];
 
@@ -14,15 +15,15 @@ try {
     //Server settings
     $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
     $mail->isSMTP();                                            // Send using SMTP
-    $mail->Host       = 'smtp-mail.outlook.com';                    // Set the SMTP server to send through
+    $mail->Host       = $MAIL_HOST;                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'puamsistema@outlook.com';                     // SMTP username
-    $mail->Password   = 'ucepuam12';                               // SMTP password
+    $mail->Username   = $MAIL_USER;                     // SMTP username
+    $mail->Password   = $MAIL_PASS;                               // SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-    $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+    $mail->Port       = $MAIL_PORT;                                    // TCP port to connect to
 
     //Recipients
-    $mail->setFrom('puamsistema@outlook.com', 'Sistema administrativo');
+    $mail->setFrom($MAIL_USER, 'Sistema administrativo');
     $mail->addAddress($email);     // Add a recipient
     //$mail->addAddress('ellen@example.com');               // Name is optional
     //$mail->addReplyTo('info@example.com', 'Information');
@@ -36,7 +37,7 @@ try {
     // Content
     $mail->isHTML(true);                                  // Set email format to HTML
     $mail->Subject = 'Credenciales Sistema PUAM';
-    $mail->Body    = 'Bienvenido al Sistema PUAM \n Para ingresar utiliza tu correo institucional y la siguiente  contraseña: 123456 en http://localhost:8888/PUAM/';
+    $mail->Body    = 'Bienvenido al Sistema PUAM \n Para ingresar utiliza tu correo institucional y la siguiente  contraseña: ' . $MAIL_PASS_DEFAULT . 'en ' . $DOMINIO;
     //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
     $mail->SMTPDebug = false;
     $mail->do_debug = false;
